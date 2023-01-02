24e63ab77fc82b20766310bcb0c3a1fc939f8c42
Marcin Dołowy <marcindolowy00@gmail.com>
2023-01-02 17:05:59 +0100

    Refactor DataReader.cs

--- library-management-system/model/User.cs
+++ library-management-system/model/User.cs
@@ -22,7 +22,7 @@ public abstract class User : ICsvConvertible
     {
         if (ReferenceEquals(null, obj)) return false;
         if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
+        if (obj.GetType() != GetType()) return false;
         return Equals((User)obj);
     }
 
